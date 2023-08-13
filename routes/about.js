e1fa5acf48a8060b1a5ed3a4a283ad14e1322c07
frametutor <142149229+frametutor@users.noreply.github.com>
2023-08-13 18:08:09 +0500

    Update about.js/ route

--- routes/about.js
+++ routes/about.js
@@ -5,5 +5,7 @@ const abt_controller = require('../controllers/about_controller');
 
 router.get('/', abt_controller.getAbout);
 router.put('/update' , abt_controller.updateAbout);
+router.post('/create' , abt_controller.crateAbout);
+
 
 module.exports = router; 
